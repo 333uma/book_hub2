6b8b773e155abe0376450592ca212526746dd0e7
umamaheswari <numamaheswari02@gmail.com>
2023-12-18 21:32:35 +0530

    first commit

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const {open} = require("sqlite");
 const sqlite3 = require("sqlite3");
 const books = require('./books.json');
 const dbPath = path.join(__dirname,'books.db');
+const cors = require("cors");
+app.use(cors());
 
 let db = null;
 
